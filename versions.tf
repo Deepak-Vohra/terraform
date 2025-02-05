59bad6b7b57c4c0a6af5a1feee3285c30853b3c4
Deepak-Vohra <dvohra10@yahoo.com>
2025-02-05 07:51:42 -0800

    Create versions.tf

---
+++ versions.tf
@@ -0,0 +1,13 @@
+terraform{
+   required_providers{
+      example ={
+         version =">= 1.0.0"
+         source ="terraform-example.com/exampleprovider/example"
+      }
+   }
+   required_version =">= 1.1.0"
+}
+
+provider "example"{
+   # ...
+}
